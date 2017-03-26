33dfef82e57eae618bd994dd7989652e3d3c43e8
Ireoo <s@ireoo.com>
2017-03-27 03:42:54 +0800

    init

--- spider.js
+++ spider.js
@@ -10,129 +10,76 @@ var Spider = function(opts) {
 };
 
 Spider.prototype.run = function(rules) {
-    var r = rules || this.rules;
-    if (this.isArray(r)) {
-        r.forEach(function(v) {
-            if (this.isArray(v.rules)) {
-                v.rules.forEach(function(rule) {
-                    if (rule.list) {
-                        if (rule.rule.url) {
-                            this.list(v, function(list) {
-                                this.run(list);
-                            });
-                        } else {
-                            console.log("列表中不含网址规则,无法继续操作!");
-                        }
-                    } else {
-                        this.one(v, function(data) {
-                            this.callback(data);
-                        });
-                    }
-                });
-            } else {
-                if (v.rules.list) {
-                    if (v.rules.rule.url) {
-                        this.list(v.rules, function(list) {
-                            this.run(list);
+    this.once(rules || this.rules, function(once) {
+        this.get(once.url, function(jQuery) {
+            this.once(once.rules, function(one) {
+                if (one.list) {
+                    if (one.rule.url) {
+                        this.list(one, jQuery, function(rule) {
+                            this.run(rule);
                         });
                     } else {
                         console.log("列表中不含网址规则,无法继续操作!");
                     }
                 } else {
-                    this.one(v.rules, function(data) {
-                        this.callback(data);
-                    });
-                }
-            }
-        });
-    } else {
-        if (r.rules.constructor == Array) {
-            r.rules.forEach(function(rule) {
-                if (rule.list) {
-                    if (rule.rule.url) {
-                        this.list(rule, function(list) {
-                            this.run(list);
-                        });
-                    } else {
-                        console.log("列表中不含网址规则,无法继续操作!");
-                    }
-                } else {
-                    this.one(rule, function(data) {
+                    this.one(one, jQuery, function(data) {
                         this.callback(data);
                     });
                 }
             });
-        } else {
-            if (rules.list) {
-                if (rules.rule.url) {
-                    this.list(rules, function(list) {
-                        this.run(list);
-                    });
-                } else {
-                    console.log("列表中不含网址规则,无法继续操作!");
-                }
-            } else {
-                this.one(rules, function(data) {
-                    this.callback(data);
-                });
-            }
-        }
-    }
-};
-
-Spider.prototype.list = function(rule, cb) {
-    this.get(r.url, function($) {
-        $(rule.list).each(function(i, v) {
-            var one = {};
-            for (var k in rule.rule) {
-                switch (rule.rule[k].type) {
-                    case 'text':
-                        one[k] = v.find(rule.rule[k].text).text();
-                        break;
-
-                    case 'html':
-                        one[k] = v.find(rule.rule[k].text).html();
-                        break;
-
-                    case 'val':
-                        one[k] = v.find(rule.rule[k].text).val();
-                        break;
-
-                    default:
-                        one[k] = v.find(rule.rule[k].text).attr(rule.rule[k].type);
-                        break;
-                }
-            }
-            rule.link.url = one.url;
-            cb(rule);
         });
     });
 };
 
-Spider.prototype.one = function(rule, cb) {
-    this.get(r.url, function($) {
+Spider.prototype.list = function(rule, $, cb) {
+    $(rule.list).each(function(i, v) {
         var one = {};
         for (var k in rule.rule) {
             switch (rule.rule[k].type) {
                 case 'text':
-                    one[k] = $(rule.rule[k].text).text();
+                    one[k] = v.find(rule.rule[k].text).text();
                     break;
 
                 case 'html':
-                    one[k] = $(rule.rule[k].text).html();
+                    one[k] = v.find(rule.rule[k].text).html();
                     break;
 
                 case 'val':
-                    one[k] = $(rule.rule[k].text).val();
+                    one[k] = v.find(rule.rule[k].text).val();
                     break;
 
                 default:
-                    one[k] = $(rule.rule[k].text).attr(rule.rule[k].type);
+                    one[k] = v.find(rule.rule[k].text).attr(rule.rule[k].type);
                     break;
             }
         }
-        cb(one);
+        rule.link.url = one.url;
+        cb(rule);
     });
+};
+
+Spider.prototype.one = function(rule, $, cb) {
+    var one = {};
+    for (var k in rule.rule) {
+        switch (rule.rule[k].type) {
+            case 'text':
+                one[k] = $(rule.rule[k].text).text();
+                break;
+
+            case 'html':
+                one[k] = $(rule.rule[k].text).html();
+                break;
+
+            case 'val':
+                one[k] = $(rule.rule[k].text).val();
+                break;
+
+            default:
+                one[k] = $(rule.rule[k].text).attr(rule.rule[k].type);
+                break;
+        }
+    }
+    cb(one);
 };
 
 Spider.prototype.get = function(url, cb) {
@@ -143,6 +90,20 @@ Spider.prototype.get = function(url, cb) {
             console.log(err);
         }
     });
+};
+
+Spider.prototype.more = function(list, cb) {
+
+};
+
+Spider.prototype.once = function(more, cb) {
+    if (this.isArray(more)) {
+        more.forEach(function(once) {
+            cb(once);
+        });
+    } else {
+        cb(more);
+    }
 };
 
 Spider.prototype.isArray = function(object) {
